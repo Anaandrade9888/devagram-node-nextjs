30c6dd528ec0520521e572f75ca075160df33d10
Anaandrade9888 <anaandrade9888@gmail.com>
2024-07-29 15:52:42 +0100

    pesquisa de usuarios concluida

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -3,8 +3,47 @@ import type { RespostaPadraoMsg } from '@/types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT'
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
 import { UsuarioModel } from '@/models/UsuarioModel';
+import nc from 'next-connect';
+import { upload,uploadImagemCosmic } from '@/services/uploadImagemCosmic';
 
-const usuarioEndpoint = async (req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
+const handler = nc()
+   .use(upload.single('file'))
+   .put(async(req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
+    try{
+        
+        const {userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+         
+        if(!usuario){
+            return res.status(400).json({erro : 'Usuario nao encontrado'});
+        }
+
+        const{nome} = req?.body;
+        if(!nome && nome.length > 2){
+            usuario.nome = nome;
+        }
+
+        const {file} = req;
+        if(file && file.originalname){
+            
+            const image = await uploadImagemCosmic(req);
+            if(file && image.media && image.media.url){
+                usuario.avatar = image.media.url;
+            }
+        }
+
+        await UsuarioModel
+        .findByIdAndUpdate({_id : usuario._id}, usuario);
+
+        return res.status(200).json({msg : 'Usuario alterado com sucesso'});
+
+    }catch(e){
+        console.log(e);
+        return res.status(400).json({erro : 'Nao foi possivel atualixar o usuario' + e})
+    }
+})
+
+.get(async (req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
     try{
     const {userId} = req?.query;
     const usuario = await UsuarioModel.findById(userId);
@@ -16,6 +55,12 @@ const usuarioEndpoint = async (req : any, res : NextApiResponse<RespostaPadraoMs
     }
    
    return res.status(400).json({erro : 'Nao foi possivel obter dados do usuario'});
+});
+ 
+export const config ={
+    api : {
+        bodyParser : false
+    }
 }
 
-export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
+export default validarTokenJWT(conectarMongoDB(handler));
